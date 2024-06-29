e1090a3d5c702545f07a15315eea5d412b326439
Hayrapetyan Hrayr <hrayr.hayrapetyan02@gmail.com>
2024-06-29 15:28:31 +0400

    Hayrapetyan Hrayr

--- Majority_Element.py
+++ Majority_Element.py
@@ -1,11 +1,21 @@
 class Solution:
     def majorityElement(self, nums: list[int]) -> int:
-        major_index=0
-        for i in range(0,len(nums)-1):
-            count=0
-            for j in range(i+1,len(nums)):
-                if nums[i]==nums[j]:
-                    count+=1
-            if count>=len(nums)/2:
-                major_index=i
-        return nums[major_index]
+        di = {}
+        major_key = nums[0]
+
+        for i in nums:
+            if i in di.keys():
+                di[i] += 1
+            else:
+                di[i] = 1
+        print(di)
+
+        for i, j in di.items():
+            if j > di[major_key]:
+                major_key = i
+
+        return major_key
+
+
+
+
