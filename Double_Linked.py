6d45ef9eb5fff4a9eb370531485fe98dc9d56e91
davitmartirosyan <phpencoding@gmail.com>
2024-08-05 16:34:01 +0400

    Hayrapetyan Hrayr

---
+++ Double_Linked.py
@@ -0,0 +1,154 @@
+
+class Node:
+    def __init__(self,data=None):
+        self.next=None
+        self.prev=None
+        self.data=data
+
+class Double_Linked:
+    def __init__(self):
+        self.head=None
+        self.tail=None
+
+    def push_Front(self, data):
+
+        newNode = Node(data)
+        if self.head is  None:
+            self.head =self.tail=newNode
+        else:
+            newNode.next = self.head
+            self.head.prev=newNode
+            self.head=newNode
+
+    def pop_Front(self):
+        if self.head==None:
+            return None
+        if self.head==self.tail:
+            self.head=self.tail=None
+        self.head=self.head.next
+        self.head.prev=None
+
+    def push_Back(self,data):
+
+        newNode=Node(data)
+        if self.tail is None:
+            self.head=self.tail=None
+        else:
+            self.tail.next=newNode
+            newNode.prev=self.tail
+            self.tail=newNode
+    def pop_Back(self):
+
+        if self.head is None:
+            raise IndexError('List Is Empty')
+
+        if self.head==self.tail:
+            self.head=self.tail=None
+        else:
+            self.tail=self.tail.prev
+            self.tail.next=None
+
+    def insert_After(self, prev_Node, data):
+
+        curr=self.head
+        newNode=Node(data)
+
+        while curr is not None and curr.data!=prev_Node.data:
+            curr=curr.next
+
+        if curr is None:
+            raise ValueError('The Given Node Is Not In The List')
+
+        newNode.next=curr.next
+        newNode.prev=curr
+        curr.next=newNode
+
+        if newNode.next is None:
+            self.tail=newNode
+
+
+    def clear(self):
+        self.head=self.tail=None
+    def isEmpty(self):
+        return self.head is None
+
+    def front(self):
+        return self.head
+
+    def reverse(self):
+        prev = None
+        curr = self.head
+
+        while curr is not None:
+            next_node = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_node
+
+        self.head = prev
+
+    def merge_Other(self,ls):
+
+        if self.head is None or ls.head is None:
+                raise ValueError('Lists Should Not Be Empty')
+
+        self.tail.next=ls.head
+        ls.head.prev=self.tail
+        self.tail=ls.tail
+    def sort(self):
+
+        swapped = True
+        while swapped:
+            swapped = False
+            curr = self.head
+
+            while curr.next:
+                if curr.data > curr.next.data:
+                    curr.data, curr.next.data = curr.next.data, curr.data
+                    swapped = True
+                curr = curr.next
+
+    def remove_Duplicate(self):
+
+        curr=self.head
+        while curr is not None:
+            tmp=curr
+            while tmp.next is not None:
+                if tmp.next.data==curr.data:
+                    tmp.next=tmp.next.next
+                else:
+                    tmp=tmp.next
+            curr=curr.next
+
+    def print_List(self):
+
+        curr=self.head
+
+        while curr!=None:
+            print(curr.data,end=' ')
+            curr=curr.next
+        print()
+
+if __name__=='__main__':
+
+    ls=Double_Linked()
+    ls.push_Front(45)
+    ls.push_Front(435)
+    ls.push_Front(78)
+    ls.push_Front(12)
+    ls.push_Front(89)
+    ls.push_Back(100)
+    ls.print_List()
+    ls.pop_Back()
+    ls2=Double_Linked()
+    ls2.push_Front(12)
+    ls2.push_Front(678)
+    ls2.push_Front(0)
+    ls2.print_List()
+    ls.sort()
+    ls.print_List()
+    ls.merge_Other(ls2)
+    ls.print_List()
+
+    ls.insert_After(Node(435),10)
+    ls.print_List()
